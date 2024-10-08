9557b5493daf9472382d6c839a344f3e563846c0
ElioPeraza <perazaelio@gmail.com>
2024-10-07 21:04:42 -0300

    second update

--- pet-store/src/componentes/MyNavbar.jsx
+++ pet-store/src/componentes/MyNavbar.jsx
@@ -1,46 +1,78 @@
-import Button from 'react-bootstrap/Button';
-import Container from 'react-bootstrap/Container';
-import Form from 'react-bootstrap/Form';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
+import { Form, Button, Navbar, Nav, Container, Row, Col, Dropdown, DropdownButton } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../App.css';
 
+const expand = 'lg';
 
 const MyNavbar = () => {
   return (
-    
-    <Navbar className='Navbar'>
+    <Navbar key={expand} expand={expand} className="Navbar mb-3">
       <Container fluid>
-        <Navbar.Brand href="#">Huella Animal</Navbar.Brand>
-        <Navbar.Toggle aria-controls="navbarScroll" />
-        <Navbar.Collapse id="navbarScroll">
-          <Nav
-            className="me-auto my-2 my-lg-0"
-            style={{ maxHeight: '100px' }}
-            navbarScroll
-          >
-            <Nav.Link href="#action1">Perros</Nav.Link>
-            <Nav.Link href="#action1">Gatos</Nav.Link>
-            <Nav.Link href="#action1">Peses</Nav.Link>
-            <Nav.Link href="#action1">Pequeñas Mascotas</Nav.Link>
-            <Nav.Link href="#action2">Ofertas</Nav.Link>
+        <Row className="w-100">
+          
+          <Col xs={12} className="d-flex align-items-center justify-content-between">
             
-           
-          </Nav>
-          <Form className="d-flex">
-            <Form.Control
-              type="search"
-              placeholder="Busca aqui el producto preferido de tu mascota"
-              className="me-2"
-              aria-label="Search"
-            />
-            <Button variant="outline-success"><i class="fa-solid fa-magnifying-glass"></i></Button>
-          </Form>
-        </Navbar.Collapse>
+            <Navbar.Brand href="#">
+              <img
+                src="assets/img/logo.png" // Ruta del logo
+                alt="Huella Animal"
+                style={{ width: '100px', height: 'auto' }} // Tamaño ajustable del logo
+              />
+            </Navbar.Brand>
+
+            
+            <DropdownButton
+              id="dropdown-basic-button"
+              title="Menú"
+              className="d-lg-none" 
+            >
+              <Dropdown.Item href="#action1"><i className="fa-solid fa-dog"></i> Perros</Dropdown.Item>
+              <Dropdown.Item href="#action2"><i className="fa-solid fa-cat"></i> Gatos</Dropdown.Item>
+              <Dropdown.Item href="#action3"><i className="fa-solid fa-fish"></i> Peces</Dropdown.Item>
+              <Dropdown.Item href="#action5"><i className="fa-solid fa-percent"></i> Ofertas</Dropdown.Item>
+            </DropdownButton>
+
+            {/* Enlaces en la misma fila para pantallas grandes */}
+            <Nav className="d-none d-lg-flex">
+              <Nav.Link className='Link' href="#action1">
+                <i className="fa-solid fa-dog"></i> Perros
+              </Nav.Link>
+              <Nav.Link className='Link' href="#action2">
+                <i className="fa-solid fa-cat"></i> Gatos
+              </Nav.Link>
+              <Nav.Link className='Link' href="#action3">
+                <i className="fa-solid fa-fish"></i> Peces
+              </Nav.Link>
+              <Nav.Link className='Link' href="#action5">
+              <i className="fa-solid fa-percent"></i> Ofertas
+              </Nav.Link>
+            </Nav>
+          </Col>
+
+          {/* Barra de búsqueda en una fila separada */}
+          <Col xs={12} className="mt-1">
+            <Form className="d-flex justify-content-center">
+              <Form.Control
+                type="search"
+                placeholder="Busca aquí el producto preferido de tu mascota"
+                className="me-2"
+                aria-label="Search"
+                style={{ maxWidth: '700px', width: '100%' }} 
+              />
+              <Button variant="outline-success">
+                <i className="fa-solid fa-magnifying-glass"></i>
+              </Button>
+            </Form>
+          </Col>
+        </Row>
       </Container>
     </Navbar>
   );
 }
 
 export default MyNavbar;
+
+
+
+
+
