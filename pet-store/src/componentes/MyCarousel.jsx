c3b31f737a85945a82c534f3f5744f671e70c944
ElioPeraza <perazaelio@gmail.com>
2024-10-07 21:24:04 -0300

    update

--- pet-store/src/componentes/MyCarousel.jsx
+++ pet-store/src/componentes/MyCarousel.jsx
@@ -8,15 +8,15 @@ import '../App.css';
 const MyCarousel = () => {
     const ArrayDeImagenes = [
         {
-            Imagen: "https://cl.pinterest.com/pin/567594359298001179/",
+            Imagen: "https://www.superzoo.cl/on/demandware.static/-/Library-Sites-SuperZooSharedLibrary/default/dwe3e7a685/SuperZoo/homepage/2022/main_banner/delivery-express-desktop.jpg",
             Nombre:"Producto 1"  
         },
         {
-            Imagen:"https://cl.pinterest.com/pin/567594359298001179/",
+            Imagen:"https://www.superzoo.cl/on/demandware.static/-/Library-Sites-SuperZooSharedLibrary/default/dw942f60ae/SuperZoo/homepage/2024/main_banner/imbatibles/imbatibles-hills-desktop.jpg",
             Nombre:"Producto 2"
         },
         {
-            Imagen:"https://cl.pinterest.com/pin/567594359298001179/",
+            Imagen:"https://www.superzoo.cl/on/demandware.static/-/Library-Sites-SuperZooSharedLibrary/default/dw1000ffb0/SuperZoo/homepage/2024/main_banner/imbatibles/imbatibles-leonardo-desktop.jpg",
             Nombre:"Producto 3"
         },
     ];
@@ -26,7 +26,7 @@ const MyCarousel = () => {
             {ArrayDeImagenes.map((Element,) => {
                 return (
                     <Carousel.Item  className='ItemCarrusel'>
-                        <img className="ImagenCarrusel" src={Element.Imagen} alt={Element.Nombre} />
+                        <img className="ImagenCarrusel" src={Element.Imagen} />
                         <Carousel.Caption>
                             <h5>{Element.Nombre}</h5>
                         </Carousel.Caption>
